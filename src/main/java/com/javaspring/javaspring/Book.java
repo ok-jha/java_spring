914aeea0bac3d39bce5a39d0b632c774927da785
Om Jha <om_jha@Oms-MacBook-Air.local>
2023-03-27 09:22:00 +0530

    First CRUD using Spring Boot

---
+++ src/main/java/com/javaspring/javaspring/Book.java
@@ -0,0 +1,22 @@
+package com.javaspring.javaspring.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+
+    // Getters and setters
+    // ...
+}
+
---
+++ src/main/java/com/javaspring/javaspring/BookRepository.java
@@ -0,0 +1,4 @@
+package com.javaspring.javaspring;
+
+public interface BookRepository {
+}
